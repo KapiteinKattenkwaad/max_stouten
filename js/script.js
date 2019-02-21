829163eb88a01e577e806f603407ac605bfa249a
Max St <maxstouten@hotmail.com>
2019-02-21 07:24:05 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("hamburger-menu").style.zIndex = "0"
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("hamburger-menu").style.zIndex = "1";
+  }
